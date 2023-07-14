17782255c9a14a0420ac50597097add9b6dbf6f8
anaelsouza <appleanael@gmail.com>
2023-07-13 21:39:40 -0300

    versao inicial

---
+++ 3_Repeticoes/repeticoes01.js
@@ -0,0 +1,15 @@
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('oi')
+// console.log('Você esqueceu que eu existo?')
+
+
+for (let x = 0; x<=7; x = x + 1){
+    console.log('oi')
+}
+console.log('voce esqueceu que eu existo?')
